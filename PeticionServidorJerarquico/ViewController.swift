12ae04a47e38e665fcc9147a8869f889345fe39c
jessimr <jessicamedozaruiz@gmail.com>
2017-03-25 09:55:42 +0100

    Initial Commit

---
+++ PeticionServidorJerarquico/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeticionServidorJerarquico
+//
+//  Created by JESSICA MENDOZA RUIZ on 25/03/2017.
+//  Copyright © 2017 JESSICA MENDOZA RUIZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
